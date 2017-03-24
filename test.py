1ccd44810abaab13df0b07d47b365a5002201d79
Harrison Beachey <hbeachey@nd.edu>
2017-03-24 15:10:39 -0400

    add some basic unit tests

--- test.py
+++ test.py
@@ -1,29 +1,21 @@
-import json
-import serviceHandler
-import joinHandler
 from hesburgh import heslog
+import unittest
+import os
+from test import AlephTest, IlliadTest
 
-def debug(name, func, event=None, context=None):
-  heslog.test("\n== %s ==" % name)
-  out = func(event, context)
-  heslog.test("------ func returns with:")
-  heslog.test(json.dumps(out, indent = 2))
-  heslog.test("== ~%s ==\n" % name)
+modules = [AlephTest, IlliadTest]
 
-event = {"type": "pending", "netid": "hbeachey"}
-debug("aleph - pending", serviceHandler.aleph, event)
-debug("illiad - pending", serviceHandler.illiad, event)
+tests = []
+for module in modules:
+  tests.append(module.Suite())
 
-event = {"type": "borrowed", "netid": "hbeachey"}
-debug("aleph - borrowed", serviceHandler.aleph, event)
-debug("illiad - borrowed", serviceHandler.illiad, event)
+alltests = unittest.TestSuite(tests)
 
-event = {
-  "requestContext": {
-    "authorizer": {
-      "netid": "hbeachey",
-    },
-  },
-}
-debug("borrowed", joinHandler.borrowed, event)
-debug("pending", joinHandler.pending, event)
+
+if __name__ == '__main__':
+  # Set seret for encode/decode to use
+  os.environ["JWT_SECRET"] = 'secret'
+  # Hide most (if not all) heslogs
+  heslog.setLevels(heslog.LEVEL_TEST)
+
+  unittest.TextTestRunner(verbosity=2).run(alltests)
