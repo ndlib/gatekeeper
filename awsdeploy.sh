cc937cbfc4eba5993ee87bdb28b061a91449d71d
Harrison Beachey <hbeachey@nd.edu>
2017-04-24 15:55:32 -0400

    clean up some more env vars

--- awsdeploy.sh
+++ awsdeploy.sh
@@ -2,7 +2,9 @@
 if [ -z ${1+x} ]; then echo "Stage name required. Ex: $0 dev testlibnd-serverless"; exit; else echo "Deploying to stage '$1'"; fi
 if [ -z ${2+x} ]; then echo "Deploy bucket required. Ex: $0 dev testlibnd-serverless"; exit; else echo "Deploying to bucket '$2'"; fi
 if [ -z ${ILLIAD_KEY+x} ]; then echo "Environment variable ILLIAD_KEY required"; exit; fi
+if [ -z ${ILLIAD_URL+x} ]; then echo "Environment variable ILLIAD_URL required"; exit; fi
 if [ -z ${ALEPH_PATH+x} ]; then echo "Environment variable ALEPH_PATH required"; exit; fi
+if [ -z ${ALEPH_URL+x} ]; then echo "Environment variable ALEPH_URL required"; exit; fi
 if [ -z ${AWS_ROLE_ARN+x} ]; then echo "Environment variable AWS_ROLE_ARN required"; exit; fi
 
 STAGE_NAME=$1
@@ -16,9 +18,9 @@ KMS_ARN="alias/portalResources-$STAGE_NAME"
 ALEPH_PATH=`aws kms encrypt --key-id $KMS_ARN --plaintext $ALEPH_PATH --query CiphertextBlob --output text`
 aws lambda update-function-configuration \
   --function-name portalResources-$STAGE_NAME-aleph \
-  --environment "Variables={ ALEPH_PATH='$ALEPH_PATH' }"
+  --environment "Variables={ ALEPH_PATH='$ALEPH_PATH', ALEPH_URL='$ALEPH_URL' }"
 
 ILLIAD_KEY=`aws kms encrypt --key-id $KMS_ARN --plaintext $ILLIAD_KEY --query CiphertextBlob --output text`
 aws lambda update-function-configuration \
   --function-name portalResources-$STAGE_NAME-illiad \
-  --environment "Variables={ ILLIAD_KEY='$ILLIAD_KEY' }"
+  --environment "Variables={ ILLIAD_KEY='$ILLIAD_KEY', ILLIAD_URL='$ILLIAD_URL' }"
