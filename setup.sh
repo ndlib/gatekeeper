71b6a201a0bb95686b7222d651d6dd579359e948
Harrison Beachey <hbeachey@nd.edu>
2017-07-20 13:57:30 -0400

    get the specialized description field from marc

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 # copy all example files to runable local files to hold secrets
-ln -s "/usr/local/lib/python2.7/site-packages/hesburgh"
+pip install hesburgh-utilities --target hesburgh_pip_install
+
+ln -fs hesburgh_pip_install/hesburgh ./
