2550ab4fe6466cdbeeb0a764f0c139549cedde9c
Harrison Beachey <hbeachey@nd.edu>
2017-08-30 09:29:47 -0400

    remove borrowed and pending lambdas to split ill and aleph requests

--- serviceHandler.py
+++ serviceHandler.py
@@ -4,18 +4,15 @@ from serviceRequests.aleph import Aleph
 from serviceRequests.illiad import Illiad
 
 hestest.init(__file__, "testdata")
-# need to figure out what is secret in aleph + how to handle secrets in lambda
 
 requestTypes = {
   "borrowed": ["checkedOut", "web"],
   "pending": ["pending"],
+  "user": ["user"],
 }
 
 def _handle(service, requestType):
-  data = {
-    'user': service.request("user")
-  }
-
+  data = {}
   toGet = requestTypes[requestType]
   for key in toGet:
     data[key] = service.request(key)
@@ -24,40 +21,63 @@ def _handle(service, requestType):
 
 
 def _success(data):
-  heslog.info("success")
-  return data
-
-
-def _error():
+  heslog.info("Success")
   return {
-    "statusCode": 404,
+    "statusCode": 200,
+    "headers": {
+      "Access-Control-Allow-Origin" : "*", # Required for CORS support to work
+      "x-nd-version": hesutil.getEnv("VERSION", 0),
+    },
+    "body": json.dumps(data)
+  }
+
+
+def _error(code):
+  return {
+    "statusCode": code,
+    "headers": {
+      "Access-Control-Allow-Origin" : "*", # Required for CORS support to work
+      "x-nd-version": hesutil.getEnv("VERSION", 0),
+    },
   }
 
 
 def aleph(event, context):
-  requestType = event.get("type", None)
-  heslog.addContext(fn="aleph", trace=event.get("trace", ""), requestType=requestType)
-  heslog.info("Starting request")
+  requestType = event.get("queryStringParameters", {}).get("type", None)
+  netid = event.get("requestContext", {}).get("authorizer", {}).get("netid", None)
 
-  netid = event.get("netid", None)
+  heslog.addLambdaContext(event, context, fn="aleph", requestType=requestType)
+
   if netid is None:
     heslog.error("no netid")
-    return _error()
+    return _error(401)
 
+  if requestType is None:
+    heslog.error("no query type specified")
+    return _error(400)
+
+  heslog.info("Starting request")
   data = _handle(Aleph(netid), requestType)
+  heslog.info("Success")
   return _success(data)
 
 
 def illiad(event, context):
-  requestType = event.get("type", None)
-  heslog.addContext(fn="illiad", trace=event.get("trace", ""), requestType=requestType)
-  heslog.info("Starting request")
+  requestType = event.get("queryStringParameters", {}).get("type", None)
+  netid = event.get("requestContext", {}).get("authorizer", {}).get("netid", None)
 
-  netid = event.get("netid", None)
+  heslog.addLambdaContext(event, context, fn="illiad", requestType=requestType)
+
   if netid is None:
     heslog.error("no netid")
-    return _error()
+    return _error(401)
 
+  if requestType is None:
+    heslog.error("no query type specified")
+    return _error(400)
+
+  heslog.info("Starting request")
   data = _handle(Illiad(netid), requestType)
+  heslog.info("Success")
   return _success(data)
 
