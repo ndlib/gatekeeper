46fcb3a50dca6a718194a94868fcaba19cf8c1cb
Harrison Beachey <hbeachey@nd.edu>
2017-12-19 13:39:00 -0500

    update findItem to include more aleph information

--- alephQuery.py
+++ alephQuery.py
@@ -1,5 +1,6 @@
 from hesburgh import heslog, hesutil
 from serviceRequests.aleph import Aleph
+from string import punctuation
 import json
 
 def _error(code):
@@ -21,9 +22,25 @@ def _success(data):
     },
   }
 
+# helper so we don't have to have if logic on every field below
+def iterate(field):
+  if isinstance(field, list):
+    for i in field:
+      yield i
+  else:
+    yield field
+
+# helper to get first (or only) element of a field
+def first(field):
+  for x in iterate(field):
+    return x
+
+
 def findItem(event, context):
   itemId = event.get("pathParameters", {}).get("systemId")
   heslog.addLambdaContext(event, context, sysId=itemId)
+
+  outData = {}
 
   if not itemId:
     heslog.error("No system id provided")
@@ -31,42 +48,60 @@ def findItem(event, context):
 
   aleph = Aleph(None)
   parsed = aleph.findItem(itemId)
-  heslog.info("Got %s from aleph" % parsed)
+  heslog.info("Got response from aleph")
   if not parsed:
     heslog.error("Nothing in parsed information")
     return _error(500)
 
   record = parsed.get("record", {}).get("metadata", {}).get("oai_marc", {})
-  name = record.get("varfield_245_0", {})
-  if isinstance(name, list):
-    name = name[0]
-  name = name.get("subfield_a", "").strip()
 
-  # Description is more complicated. We need 520 which could just be 1 record or multiple
-  # If there's a 520 with subfield 9 of value g, that's the one we want
+  # name
+  outData["name"] = record.get("varfield_245_0", {}).get("subfield_a", "").strip()
+
+  # description
   recordDescription = record.get("varfield_520", {})
-  description = None
-  if isinstance(recordDescription, list):
-    for i in recordDescription:
-      if i.get("subfield_9", "") == "g":
-        description = i
-        break
-    if not description:
-      description = recordDescription[0]
-  else:
-    description = recordDescription
-  description = description.get("subfield_a", "").strip()
+  description = first(recordDescription)
+  for instance in iterate(recordDescription):
+    # If there's a 520 with subfield 9 of value g, that's the one we want
+    if instance.get("subfield_9", "") == "g":
+      description = instance
+      break
+  outData["description"] = description.get("subfield_a", "").strip()
 
-  url = record.get("varfield_856_0", {})
-  if isinstance(url, list):
-    url = url[0]
-  url = url.get("subfield_u", "").strip()
+  # url
+  outData["url"] = first(record.get("varfield_856_0", {})).get("subfield_u", "").strip()
 
-  return _success({
-      "name": name,
-      "description": description,
-      "purl": url,
-    })
+  # access data
+  access = record.get("varfield_506", {})
+  for letter in ['f', 'a', 'c']:
+    for a in iterate(access):
+      accessValue = a.get("subfield_" + letter)
+      if accessValue:
+        outData["access"] = "%s%s\n" % (outData.get("access", ""), accessValue.strip())
+  outData["access"] = outData.get("access", "").strip()
+
+  # restrictions
+  outData["restrictions"] = record.get("varfield_540", {}).get("subfield_a", "").strip()
+
+  # includes
+  for inc in iterate(record.get("varfield_740_2", {})):
+    outData["includes"] = "%s%s\n" % (outData.get("includes", ""), inc.get("subfield_a", "").strip().strip(punctuation))
+  outData["includes"] = outData.get("includes", "").strip().title().strip(punctuation)
+
+  # meta (platform, publisher, provider)
+  for meta in iterate(record.get("varfield_710")):
+    sub4 = meta.get("subfield_4", "")
+    metaValue = meta.get("subfield_a", "").strip()
+
+    if sub4 == "pltfrm":
+      outData["platform"] = metaValue
+    elif sub4 == "pbl":
+      outData["publisher"] = metaValue
+    elif sub4 == "prv":
+      outData["provider"] = metaValue
+
+  heslog.info("Returning success")
+  return _success(outData)
 
 
 def renewItem(event, context):
