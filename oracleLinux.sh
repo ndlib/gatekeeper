a805aa2b755868a64650145876e852846628f006
Harrison Beachey <hbeachey@nd.edu>
2018-06-08 09:14:22 -0400

    update sync scripts

--- oracleLinux.sh
+++ oracleLinux.sh
@@ -3,4 +3,4 @@
 rm -rf lib
 mkdir lib
 
-aws s3 sync s3:// lib
+aws s3 sync s3://libnd-oracle-binary-bucket-oraclebinarybucket-1omf1vksdphrq/oracle_linux lib
--- oracleMac.sh
+++ oracleMac.sh
@@ -3,6 +3,6 @@
 rm -rf lib
 mkdir lib
 
-aws s3 sync s3:// lib
+aws s3 sync s3://libnd-oracle-binary-bucket-oraclebinarybucket-1omf1vksdphrq/oracle_mac lib
 
 pip install cx_oralce -t lib
