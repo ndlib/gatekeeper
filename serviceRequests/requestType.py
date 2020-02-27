04f85c4ca1915d3c0161b958aaa80c84ebbf76f3
Michael Kirkpatrick <mkirkpatrick@nd.edu>
2020-02-27 09:55:44 -0500

    Add constants

--- serviceRequests/requestType.py
+++ serviceRequests/requestType.py
@@ -3,6 +3,9 @@ import urllib2
 import socket
 
 class RequestType(object):
+  MAX_RETRIES = 2
+  URL_TIMEOUT = 8 #  seconds
+
   def __init__(self, netid):
     super(RequestType, self).__init__()
     self.netid = netid
@@ -19,13 +22,13 @@ class RequestType(object):
     req = urllib2.Request(url, None, headers)
     response = ""
     try:
-      response = urllib2.urlopen(req, timeout=8)
+      response = urllib2.urlopen(req, timeout=self.URL_TIMEOUT)
     except urllib2.HTTPError as e:
       heslog.error("%s" % e.code)
       heslog.error(e.read())
       return None
     except urllib2.URLError as e:
-      if isinstance(e.reason, socket.timeout) and retryCount < 2:
+      if isinstance(e.reason, socket.timeout) and retryCount < self.MAX_RETRIES:
         heslog.info('Request timed out. Retrying.')
         return self._makeReq(url, headers, retryCount + 1)
       else:
